bf3b20a0d8f3a53d779a1687ca6b8c8f0cb72bdc
Alexander <46108494+TeutonStudio@users.noreply.github.com>
2025-11-15 18:26:12 +0100

    Refactor config loading and fix level naming
    Updated configuration loading and event handling logic. Changed 'primär' to 'primar' and 'sekundär' to 'sekundar' for consistency.

--- startup.lua
+++ startup.lua
@@ -6,12 +6,6 @@ local VR = require("libraries/vektor") -- Vektorraum Rechnung
 local RV = require("libraries/rotor") -- Rotorenverwalter
 
 
--- Standard-Konfiguration (wird bei fehlender Datei erstellt)
-local defaultConfig = {
-    modem = "top", channel = 420,
-    rotors = {"front", "right", "back", "left"}
-}
-
 -- Config laden
 local cfg = CFGV.loadConfig()
 local modem = peripheral.wrap(cfg.modem)
@@ -34,7 +28,7 @@ modem.open(cfg.channel)
 while true do
     local azimuth = 0
     local steuer = vector.new()
-    if cfg.level == "primär" then 
+    if cfg.level == "primar" then 
         event, side, channel, replyChannel, quaternionHeck, dist = os.pullEvent("modem_message")
         if channel == cfg.channel then
             local quaternionLokal = ship.getQuaternion()
@@ -44,7 +38,7 @@ while true do
             print(azimuth)
         end
     end
-    if cfg.level == "sekundär" then 
+    if cfg.level == "sekundar" then 
         local quaternionHeck = ship.getQuaternion()
         modem.transmit(cfg.channel,cfg.channel,quaternionHeck)
         event, side, channel, replyChannel, quaternion, dist = os.pullEvent("modem_message")
