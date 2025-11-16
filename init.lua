d9d537d61f70383fdcad1ebec5703e10b6251d5a
Alexander <46108494+TeutonStudio@users.noreply.github.com>
2025-11-16 12:40:21 +0100

    Fix file deletion and download process in init.lua

--- init.lua
+++ init.lua
@@ -13,18 +13,19 @@ local BRANCH = "main"
 
 for _, lib in ipairs(LIBS) do
     local url = ("https://raw.githubusercontent.com/%s/%s/%s.%s"):format(REPO, BRANCH, lib.file, lib.type)
+    local name = lib.file.."."..lib.type
     
     print("Lade " .. lib.file .. "...")
 
     -- Alte Datei löschen (falls vorhanden)
-    if fs.exists(lib.file) then fs.delete(lib.file) end
+    if fs.exists(name) then fs.delete(name) end
 
     -- Pfad für Ordner anlegen, falls nicht vorhanden
     local dir = fs.getDir(lib.file)
     if dir ~= "" and not fs.exists(dir) then fs.makeDir(dir)end
 
     -- Download
-    local ok, err = pcall(shell.run, "wget", url, lib.file.."."..lib.type)
+    local ok, err = pcall(shell.run, "wget", url, name)
 
     if ok then
         print("  ✓ " .. lib.file .. " → " .. lib.file)
